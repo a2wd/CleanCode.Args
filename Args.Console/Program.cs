1b79cc70c40ccc57d10959b2060803a5a43fb5c8
a2wd <github@a2wd.com>
2017-08-04 09:57:02 +0100

    Updated test to use nunit, added initial test & example string array method

--- Args.Console/Program.cs
+++ Args.Console/Program.cs
@@ -9,12 +9,13 @@
         {
             try
             {
-                Args arg = new Args("l,p#,d*", args);
+                Args arg = new Args("l,p#,d*,a[*]", args);
                 bool logging = arg.getBoolean('l');
                 int port = arg.getInt('p');
                 string directory = arg.getString('d');
+                string[] files = arg.getStringArray('a');
 
-                executeApplication(logging, port, directory);
+                executeApplication(logging, port, directory, files);
             }
             catch(ArgsException e)
             {
@@ -22,12 +23,13 @@
             }
         }
 
-        private static void executeApplication(bool logging, int port, string directory)
+        private static void executeApplication(bool logging, int port, string directory, string[] files)
         {
             Console.WriteLine("Executed application with:");
             Console.WriteLine($"bool logging param = {logging}");
             Console.WriteLine($"int port param = {port}");
             Console.WriteLine($"string directory param = {directory}");
+            Console.WriteLine($"string array files param = {string.Join(" ",files)}");
             Console.ReadKey();
         }
     }
---
+++ Args.Tests/ArgsShould.cs
@@ -0,0 +1,25 @@
+﻿namespace Args.Tests
+{
+    using Exceptions;
+    using NUnit.Framework;
+    using System;
+
+    [TestFixture]
+    class ArgsShould
+    {
+        [Test]
+        public void BeInitializableWithNoSchemaOrArguments()
+        {
+            try
+            {
+                new Args("", new string[] { "-x" });
+                throw new Exception();
+            }
+            catch(ArgsException e)
+            {
+                Assert.AreEqual(ErrorCodes.UNEXPECTED_ARGUMENT, e.getErrorCode());
+                Assert.AreEqual('x', e.getErrorArgumentId());
+            }
+        }
+    }
+}
